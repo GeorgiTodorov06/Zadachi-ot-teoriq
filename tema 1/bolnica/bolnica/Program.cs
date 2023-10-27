b9da611fc01dacd30c06a467e12390250bd6f07b
BIQ EVREI S KUR <51885066+GeorgiTodorov06@users.noreply.github.com>
2023-10-27 09:07:24 +0300

    Add files via upload

---
+++ tema 1/bolnica/bolnica/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bolnica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int doctors = 7;
+            int treated = 0;
+            int untreated = 0;
+            for (int day = 1; day <= days; day++)
+            {
+                int patientsCount = int.Parse(Console.ReadLine());
+                if (day % 3 == 0 && untreated > treated)
+                {
+                    doctors++;
+                }
+                if (patientsCount <= doctors)
+                {
+                    treated += patientsCount;
+                }
+                else
+                {
+                    treated += doctors;
+                    untreated += (patientsCount - doctors);
+                }
+                
+            }
+            Console.WriteLine($"Treated patients: {treated}."); 
+            Console.WriteLine($"Untreated patients: {untreated}.");
+            Console.ReadKey();
+        }
+    }
+}
